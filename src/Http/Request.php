9320b982ad1ebc0562a6b3fd7c46de82ddd43acd
Kakktuss <benjaminmanderveldepro@gmail.com>
2018-08-20 21:56:27 +0200

    [Add] Request: phpdoc

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -3,78 +3,135 @@
 namespace Kiron\Http;
 
 class Request {
-    
+
+    /**
+     * @return mixed
+     */
     public static function getRequestMethod()
     {
         return $_SERVER['REQUEST_METHOD'];
     }
-    
+
+    /**
+     * @return bool
+     */
     public static function isPostRequest()
     {
         return self::getRequestMethod() === 'POST';
     }
-    
 
+
+    /**
+     * @return bool
+     */
     public static function isDeleteRequest()
     {
         return self::getRequestMethod() === 'DELETE';
     }
-    
+
+    /**
+     * @return bool
+     */
     public static function isGetRequest()
     {
         return self::getRequestMethod() === 'GET';
     }
-    
+
+    /**
+     * @return bool
+     */
     public static function isPatchRequest()
     {
         return self::getRequestMethod() === 'PATCH' || self::getRequestMethod() === 'PUT';
     }
 
+    /**
+     * @return mixed
+     */
     public static function getHost()
     {
         return $_SERVER['HTTP_HOST'];
     }
 
+    /**
+     * @return mixed
+     */
     public static function getConnection()
     {
         return $_SERVER['HTTP_CONNECTION'];
     }
 
+    /**
+     * @return mixed
+     */
     public static function getOrigin()
     {
         return $_SERVER['HTTP_ORIGIN'];
     }
 
+    /**
+     * @return mixed
+     */
     public static function getAccept()
     {
         return $_SERVER['HTTP_ACCEPT'];
     }
 
+    /**
+     * @return mixed
+     */
+    public static function getUri()
+    {
+        return $_SERVER['REQUEST_URI'];
+    }
+
+    /**
+     * @param $keyName
+     * @return bool
+     */
     public static function getHeaderKey($keyName)
     {
         return (isset($_SERVER['HTTP_'.$keyName])) ? $_SERVER['HTTP_'.$keyName] : false;
     }
-    
+
+    /**
+     * @return mixed
+     */
     public static function getLanguage()
     {
         return $_SERVER['HTTP_ACCEPT_LANGUAGE'];
     }
-    
+
+    /**
+     * @return mixed
+     */
     public static function getUserAgent()
     {
         return $_SERVER['HTTP_USER_AGENT'];
     }
-    
+
+    /**
+     * @return bool
+     */
     public static function isJson()
     {
         return strpos('application/json', $_SERVER['HTTP_ACCEPT']) !== false;
     }
-    
+
+    /**
+     * @return bool
+     */
     public static function isHtml()
     {
         return strpos('text/html', $_SERVER['HTTP_ACCEPT']) !== false;
     }
-    
-}
 
+    /**
+     * @return bool
+     */
+    public static function isPdf()
+    {
+        return strpos('application/pdf', $_SERVER['HTTP_ACCEPT']) !== false;
+    }
+}
 ?>
