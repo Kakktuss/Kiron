27757be0985cd5c057b8167f738c7c1b0e099fa2
Kakktuss <benjaminmanderveldepro@gmail.com>
2018-08-23 07:47:32 +0200

    [Edit] defines

--- src/defines.php
+++ src/defines.php
@@ -4,3 +4,7 @@ $config = Kiron\Config\Config::getInstance();
 
 define('FRAMEWORK_PATH', $config->getPathInformations()['FrameworkPath']);
 define('APPLICATION_PATH', $config->getPathInformations()['ApplicationPath']);
+define('VIEW_PATH', $config->getPathInformations()['ROOT']);
+define('LANG_PATH', $config->getPathInformations()['ROOT']);
+define('ROOT', $config->getPathInformations()['ROOT']);
+define('DS', DIRECTORY_SEPARATOR);
