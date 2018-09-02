18127be407cd85b76570c01fdbbd35c707cd56e1
Kakktuss <benjaminmanderveldepro@gmail.com>
2018-09-02 09:41:04 +0200

    [Update] MySql & PgSql support builder

---
+++ src/Database/Builder/MySql.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kiron\Database\Builder;
+
+use Kiron\Database\Builder\Builder as BaseBuilder;
+
+class MySql extends BaseBuilder {
+    
+    
+}
+
+?>
---
+++ src/Database/Builder/PgSql.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kiron\Database\Builder;
+
+class PgSql {
+    
+    
+    
+}
+
+?>
