026e78af0241d61ed05a4a9936539c050e662883
Kakktuss <benjaminmanderveldepro@gmail.com>
2018-09-06 19:08:42 +0200

    [Add] Controller Exception

---
+++ src/MVC/Exception/Controller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 05/09/2018
+ * Time: 19:53
+ */
+
+namespace Kiron\MVC\Exception;
+
+use \Exception;
+
+class Controller extends Exception
+{
+
+}
