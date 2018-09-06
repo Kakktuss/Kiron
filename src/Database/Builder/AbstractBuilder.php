98699ffab9941245bffab5450d1f39bc179d7b54
Kakktuss <benjaminmanderveldepro@gmail.com>
2018-09-06 19:07:29 +0200

    [Update] Database AbstractBuilder

--- src/Database/Builder/AbstractBuilder.php
+++ src/Database/Builder/AbstractBuilder.php
@@ -9,20 +9,34 @@ abstract class AbstractBuilder implements BuilderInterface {
     protected $database;
         
     abstract public function select($table, $columns = null) : self;
-    
+
     abstract public function insert($table, $columns) : self;
     
-    abstract public function delete($delete) : self;
-        
+    abstract public function delete(string $table, $columns);
+
     abstract public function update($update) : self;
     
     abstract public function join($join, $table, $on) : self;
-    
-    abstract public function from($table) : self;
-    
+
     abstract public function value(string $name, string $value) : self;
     
     abstract public function values(array $names, array $values) : self;
+
+    abstract public function set($columns, $values) : self;
+
+    abstract public function sets(array $columns, array $values) : self;
+
+    abstract public function appendParameters(array $names, array $values) : self;
+
+    abstract public function appendParameter(string $name, $value) : self;
+
+    abstract public function fullJoin(string $table, $firstCond, $secondCond) : self;
+
+    abstract public function innerJoin(string $table, $firstCond, $secondCond) : self;
+
+    abstract public function leftJoin(string $table, $firstCond, $secondCond) : self;
+
+    abstract public function rightJoin(string $table, $firstCond, $secondCond) : self;
     
     abstract public function where($table, $equal) : self;
     
