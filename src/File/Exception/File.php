ed760f9b3c6a164eeae16166009dd3eddeff2be6
Kakktuss <benjaminmanderveldepro@gmail.com>
2018-09-06 19:13:26 +0200

    [Add] File exception class

---
+++ src/File/Exception/File.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 03/09/2018
+ * Time: 07:49
+ */
+
+namespace Kiron\File\Exception;
+
+use \Exception;
+
+class File extends Exception
+{
+
+}
